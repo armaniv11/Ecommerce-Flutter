026f0c2196216ded74b2823c33f772b39b1f4247
vinamra <vjdjvinamra@gmail.com>
2022-01-17 19:49:41 +0530

    asdf

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:dns/controller.dart/cartController.dart';
+import 'package:dns/screens/main/main_page.dart';
+import 'package:dns/screens/splash_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+void main() async {
+  await GetStorage.init();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Get.put(CartController());
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DNS',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        canvasColor: Colors.transparent,
+        primarySwatch: Colors.blue,
+        fontFamily: "Montserrat",
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
